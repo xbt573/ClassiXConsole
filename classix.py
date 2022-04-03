54e9d91199a6eb4fac4bb6f5c00e90479722f0bf
xbt573 <alunev5@yandex.ru>
2022-04-03 18:00:06 +0300

    rework

--- classix.py
+++ classix.py
@@ -1,48 +1,51 @@
-#!/usr/bin/env python3
-
-from requests_html import HTMLSession
+import a2s
 import sys
 
-session = HTMLSession()
-
 servers = {
-    "ttt": '46.174.55.192:27016',
-    "murder": '46.174.55.192:27015',
-    "prophunt": '46.174.55.192:27018',
-    "minigames": '46.174.55.192:27017'
+        "ttt": ("46.174.55.192", 27016),
+	"murder": ("46.174.55.192", 27015),
+	"prophunt": ("46.174.55.192", 27018),
+	"minigames": ("46.174.55.192", 27017)
 }
 
-xpaths = {
-    "players": '//*[@id="HTML_num_players"]',
-    "map": '//*[@id="HTML_curr_map"]',
-    "average": '//*[@id="HTML_avg_players"]',
-    "serveraddress": '/html/body/div[2]/div[7]/div[1]/div[2]/text()[2]',
-    "manager": '/html/body/div[2]/div[7]/div[1]/div[2]/a[3]'
-}
+def getstat(servertuple, verbose=False):
+	info = a2s.info(servertuple)
 
-def getstat(server: str) -> str:
-    r = session.get(f'https://www.gametracker.com/server_info/{server}')
-    output = ""
-    
-    output += "Players: " + r.html.xpath(xpaths["players"])[0].text + "\n"
-    output += "Map: " + r.html.xpath(xpaths["map"])[0].text + "\n"
-    if ("-v" in sys.argv):
-        output += "Average (past month): " + r.html.xpath(xpaths["average"])[0].text + "\n"
-        output += "Address: " + server + "\n"
+	print("Players: " + str(info.player_count))
+	print("Map: " + info.map_name)
 
-    return output
+	if (verbose):
+		print("\n")
 
+		print("Server name:" + info.server_name)
+		print("Gamemode: " + info.game)
+		print("Max players: " + str(info.max_players))
+		print("Bot count: " + str(info.bot_count))
 
-    
+		if info.platform == "l":
+			platform = "Linux"
+		elif info.platform == "w":
+			platform = "Windows"
+		else:
+			platform = "macOS"
 
+		print("Plaform: " + platform)
+		print("Password: " + str(info.password_protected))
+		print("VAC enabled: " + str(info.password_protected))
+		print("Version: " + info.version)
 
 
 def main():
-    if (sys.argv[1] not in servers):
-        print("Wrong server name")
-        sys.exit(0)
+	if (sys.argv[1] not in servers):
+        	print("Wrong server name")
+        	sys.exit(0)
 
-    print(getstat(servers[sys.argv[1]]))
+	verbose = False
+
+	if ("-v" in sys.argv):
+		verbose = True
+
+	getstat(servers[sys.argv[1]], verbose)
 
 if __name__ == '__main__':
-    main()
+	main()
